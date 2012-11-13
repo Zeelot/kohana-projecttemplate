121ae247640752f3220985fe780c5e7bbfc0c537
Lorenzo Pisani <zeelot3k@gmail.com>
2012-11-12 22:16:42 -0800

    remove old and broken methods from Format class

--- application/classes/Format.php
+++ application/classes/Format.php
@@ -43,47 +43,6 @@ class Format {
 		return preg_replace('/^'.$regex.'$/', $format, $number_clean);
 	}
 
-	/**
-	 * Formats a URL to contain a protocol at the beginning.
-	 *
-	 * @param   string  possibly incomplete URL
-	 * @return  string
-	 */
-	public static function url($str = '')
-	{
-		// Clear protocol-only strings like "http://"
-		if ($str === '' OR substr($str, -3) === '://')
-			return '';
-
-		// If no protocol given, prepend "http://" by default
-		if (strpos($str, '://') === FALSE)
-			return 'http://'.$str;
-
-		// Return the original URL
-		return $str;
-	}
-
-	/**
-	 * Format a date using the format specified in the dates config file.
-	 *
-	 * @param   string $date        the string to format
-	 * @param   string $format      the config path in the dates config where the format is specified
-	 * @param   mixed  $default     the default value to return if the date is invalid
-	 * @param   bool   $allow_empty whether to allow an empty $date to specify 'now'
-	 * @return  mixed
-	 */
-	public static function date($date = NULL, $format = 'mysql.DATETIME', $default = NULL, $allow_empty = TRUE)
-	{
-		if ( ! $allow_empty AND ! Validate::not_empty($date))
-			return $default;
-
-		$object = date_create($date);
-
-		return ($object instanceof DateTime)
-			? $object->format(Kohana::$config->load('dates.'.$format))
-			: $default;
-	}
-
 	public static function empty_to_null($value)
 	{
 		return Valid::not_empty($value) ? $value : NULL;
