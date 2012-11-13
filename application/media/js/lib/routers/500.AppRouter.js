f33682f46fb20d8583dab45ccfe06bbb93703c6b
Lorenzo Pisani <zeelot3k@gmail.com>
2012-11-13 00:32:47 -0800

    update js files and make way for requirejs

--- application/media/js/lib/routers/500.AppRouter.js
+++
@@ -1,8 +0,0 @@
-(function ($, App, Lib) {
-	Lib.AppRouter = Backbone.Router.extend({
-		/**
-		 * This is the main router that every page uses.
-		 * Define your routes here.
-		 */
-	});
-}(jQuery, window.App, window.App.Lib));
