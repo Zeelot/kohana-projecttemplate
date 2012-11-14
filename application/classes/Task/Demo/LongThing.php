5c9ddffc64951f7aca2e9afa11f6b24fe37c3622
Lorenzo Pisani <zeelot@zeelot-ThinkPad-W530.(none)>
2012-11-14 12:26:08 -0800

    make the LongThing daemon a little more random

--- application/classes/Task/Demo/LongThing.php
+++ application/classes/Task/Demo/LongThing.php
@@ -4,6 +4,8 @@ class Task_Demo_LongThing extends Minion_Daemon {
 
 	protected function _loop(array $config)
 	{
+		// Add some random sleepyness
+		usleep(rand(0, 10) * 100000);
 		Minion_Task::factory(array('demo:thing'))->execute();
 	}
 }
